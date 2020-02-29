37ba0065bf664dbe057c9d26ce3117aab8680971
aagrosse <aagrock81@yahoo.com>
2020-02-29 15:37:13 -0500

    updated seeds and schema file

--- db/schema.sql
+++ db/schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE role(
    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
    title VARCHAR(30) UNIQUE NOT NULL,
    salary DECIMAL UNSIGNED NOT NULL,
-   department_id INT UNSIGNED NOT NULL,
+   department_id INT UNSIGNED NOT NULL
 
 );
 
@@ -20,13 +20,10 @@ CREATE TABLE employee(
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT UNSIGNED
-    INDEX role_ind (role_id),
-    CONSTRAINT fk_role FORIEGN KEY (role_id) REFERENCES role (id) ON DELETE CASCADE,
+    role_id INT UNSIGNED,
     manager_id INT UNSIGNED,
     INDEX man_ind (manager_id),
-    CONSTRAINT fk_manager FORIEGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
-);
+    );
 
 
 
